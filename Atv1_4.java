3022c5cbe60acff3255c9db1c91d9f6152be11ed
Mat-hcb0408 <matheus.henrique.campos0408@gmail.com>
2025-01-27 16:52:07 -0300

    Add files via upload

---
+++ Atv1_4.java
@@ -0,0 +1,11 @@
+package Atividades_IfElse;
+
+import java.util.Scanner;
+
+public class Atv1_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira uma idade");
+
+    }
+}
