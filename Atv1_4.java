fa7f63c0a901d03cfb61b81c9f3dcab0df1836c4
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-01-28 13:38:23 -0300

    Update Atv1_4.java

--- Atv1_4.java
+++ Atv1_4.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Atv1_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Insira uma idade");
-
+        System.out.println("Insira sua idade:");
+        int idade= sc.nextInt();
+        if (idade>=16){
+            System.out.println("Você pode votar!");
+        }else{
+            System.out.println("Você NÃo pode votar!");
+        }
     }
 }
